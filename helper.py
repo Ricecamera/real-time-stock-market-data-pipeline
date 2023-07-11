0330890f74dab598eb80f031bda213c178bbc751
Sahatsarin Pawanna <58083752+Ricecamera@users.noreply.github.com>
2023-07-11 16:18:29 +0700

    update functions for malipulating running_number

--- helper.py
+++ helper.py
@@ -1,9 +1,32 @@
 import pytz
-from datetime import datetime
+from datetime import datetime, timedelta
+
+rn_file = open('running_number.txt', 'r+')
+eastern = pytz.timezone('US/Eastern')
+
+def get_last_10m_time():
+  current_datetime = datetime.now(eastern)
+
+  # Calculate 10 minutes ago
+  ten_minutes_ago = datetime.combine(current_datetime.date(), current_datetime.time()) - timedelta(minutes=10)
+  previous_time = ten_minutes_ago.time()
+  return previous_time
 
 def is_market_open():
-    eastern = pytz.timezone('US/Eastern')
-    current_time = datetime.now(eastern).time()
-    market_open_time = datetime.strptime('09:30:00', '%H:%M:%S').time()
-    market_close_time = datetime.strptime('16:00:00', '%H:%M:%S').time()
-    return market_open_time <= current_time <= market_close_time
+  current_time = datetime.now(eastern).time()
+  market_open_time = datetime.strptime('09:30:00', '%H:%M:%S').time()
+  market_close_time = datetime.strptime('16:00:00', '%H:%M:%S').time()
+  return market_open_time <= current_time <= market_close_time
+
+def get_running_number():
+  rn_file.seek(0)
+  text = rn_file.read().strip()
+  _, i = text.split('=')
+  return int(i)
+
+def update_running_number(i: int):
+  rn_file.seek(0)
+  rn_file.write(f'running_number={i}\n')
+
+def close():
+  rn_file.close()
