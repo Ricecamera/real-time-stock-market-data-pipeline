d32eb3442b69da7af40f87177568cc61236260b2
Sahatsarin Pawanna <58083752+Ricecamera@users.noreply.github.com>
2023-07-26 00:08:43 +0700

    update duplicate message handle logic

--- kafka_consumer.py
+++ kafka_consumer.py
@@ -1,6 +1,7 @@
 from kafka import KafkaConsumer
 from s3fs import S3FileSystem
 
+from datetime import datetime
 from collections import deque
 import json
 import helper
@@ -17,7 +18,7 @@ consumer = KafkaConsumer(
   value_deserializer=lambda x: json.loads(x.decode('utf-8')))
 s3 = S3FileSystem()
 
-processed_messages = deque()
+processed_messages = dict()
 
 # Function to handle incoming messages
 def handle_message(message):
@@ -27,26 +28,38 @@ def handle_message(message):
                                                                      message.offset, message.key,
                                                                      message.value))
     data = message.value
-    
-    # remove messages date that last more than 10 mins
-    last_10_dt = helper.get_last_10m_time()
-    while processed_messages[0] < last_10_dt:
-      processed_messages.popleft()
-
-    # # Check for duplicate message
-    if data['Datetime'] == processed_messages[-1]['Datetime']:
-        print("Duplicate message detected:", data)
-        return
-
     # Process the message
-    data['stock_name'] = message.key
-    i = helper.get_running_number()
-    with s3.open("s3://socket-market-data-sahatsarin/stock_market_{}.json".format(i), 'w') as file:
-        json.dump(data, file)
+    stock_name = message.key.decode('utf-8')
+    data_date = datetime.strptime(data['Datetime'], '%Y-%m-%d %H:%M:%S')
+    
+    if (not stock_name in processed_messages):
+      processed_messages[stock_name] = deque()
+    else:
+      dq = processed_messages[stock_name]
+      skip = False
+      if len(dq) != 0:
+        # Check for duplicate message
+        if data_date == dq[-1]['Date_object']:
+          print("Duplicate message detected:", data)
+          skip = True
+          
+        # remove messages date that last more than 10 mins
+        last_10_dt = helper.get_last_10m_time()
+        while len(dq) != 0 and dq[0]['Date_object'] < last_10_dt:
+          dq.popleft()
 
-    # update running_number and add new message
-    helper.update_running_number(i+1)
-    processed_messages.append(data['Datetime'])
+      if skip:
+        return
+      
+      i = helper.get_running_number()
+      data['stock_name'] = stock_name
+      with s3.open("s3://socket-market-data-sahatsarin/stock_market_{}.json".format(i), 'w') as file:
+          json.dump(data, file)
+
+      # update running_number and add new message
+      helper.update_running_number(i+1)
+      data['Date_object'] = data_date
+      dq.append(data)
     
   except Exception as e:
     print("Error processing message:", str(e))
